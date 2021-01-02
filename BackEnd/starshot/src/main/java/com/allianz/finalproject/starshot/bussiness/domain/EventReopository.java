ebdd32f63140f92c1664635cbe74bb49b7167286
lzowmirror <51471995+lzowmirror@users.noreply.github.com>
2021-01-02 19:45:56 +0700

    Delete EventReopository.java

--- BackEnd/starshot/src/main/java/com/allianz/finalproject/starshot/bussiness/domain/EventReopository.java
+++
@@ -1,7 +0,0 @@
-package com.allianz.finalproject.starshot.bussiness.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventReopository extends CrudRepository<Event, Integer> {
-
-}
